6cdd0d78d3091aaa17107b4251166e0bbcbee9b2
ErdalErden <you@example.com>
2022-04-17 01:33:25 +0300

    Çalışıldı

--- react-admin-dashboard/src/components/topbar/Topbar.jsx
+++ react-admin-dashboard/src/components/topbar/Topbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./topbar.css"
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
 
 function Topbar() {
   return (
     <div className='topbar'>
         <div className="topbarWrapper">
             <div className="topLeft"><span className="logo">lamaadmin</span></div>
-            <div className="topRight">right</div>
+            <div className="topRight">
+              <div className="topbarIconContainer">
+                <NotificationsNone />
+                <span className="topIconBadge">2</span>
+              </div>
+              <div className="topbarIconContainer">
+                <Language />
+                <span className="topIconBadge">2</span>
+              </div>
+              <div className="topbarIconContainer">
+                <Settings />
+              </div>
+            </div>
         </div>
     </div>
   )
