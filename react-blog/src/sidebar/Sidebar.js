3dc0002687970e2387be66fd9d8cd7bda4d5de72
Erdal Erden <57586690+ErdalErden@users.noreply.github.com>
2022-05-07 14:51:13 +0300

    çalışıldı

--- react-blog/src/sidebar/Sidebar.js
+++ react-blog/src/sidebar/Sidebar.js
@@ -3,7 +3,33 @@ import "./sidebar.css"
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      sidebar
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445__340.jpg" alt="" />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, reiciendis!consectetur adipisicin
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Tech</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+        <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+        <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+        <i className="sidebarIcon fa-brands fa-pinterest-square"></i>
+        <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+        </div>
+      </div>
     </div>
   )
 }
