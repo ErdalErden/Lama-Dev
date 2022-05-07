add6e27f686abe7b578bf1de6cac5b23fe63f4b2
Erdal Erden <57586690+ErdalErden@users.noreply.github.com>
2022-05-07 13:29:27 +0300

    React Blog App Started

---
+++ react-blog/src/topbar/TopBar.js
@@ -0,0 +1,29 @@
+import "./topbar.css"
+
+function TopBar() {
+  return (
+    <div className='top'>
+        <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook-square"></i>
+        <i className="topIcon fa-brands fa-twitter-square"></i>
+        <i className="topIcon fa-brands fa-pinterest-square"></i>
+        <i className="topIcon fa-brands fa-instagram-square"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem">HOME</li>
+                <li className="topListItem">ABOUT</li>
+                <li className="topListItem">CONTACT</li>
+                <li className="topListItem">WRITE</li>
+                <li className="topListItem">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="topRight">
+            <img className="topImg" src="https://randomuser.me/api/portraits/women/29.jpg" alt="" />
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+  )
+}
+
+export default TopBar
