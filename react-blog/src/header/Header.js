85d1ac321462150439350023684d155198a691b6
Erdal Erden <57586690+ErdalErden@users.noreply.github.com>
2022-05-07 14:35:19 +0300

    çalışıldı

--- react-blog/src/header/Header.js
+++ react-blog/src/header/Header.js
@@ -3,7 +3,11 @@ import "./header.css"
 const Header = () => {
   return (
     <div className="header">
-      
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img className="headerImg" src="https://images.pexels.com/photos/8965700/pexels-photo-8965700.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
     </div>
   )
 }
